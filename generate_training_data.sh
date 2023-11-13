9b6caad4439d3a5fc7b63657daa9a1ce9bd6cff8
pakapong777 <lottiluckyme@gmail.com>
2023-11-13 11:54:23 +0700

    .github/workflows

--- generate_training_data.sh
+++ generate_training_data.sh
@@ -1,10 +1,10 @@
 rm -rf train/*
 tesstrain.sh --fonts_dir fonts \
-	     --fontlist 'Agency FB Condensed' \
-	     --lang eng \
-	     --linedata_only \
-	     --langdata_dir langdata_lstm \
-	     --tessdata_dir tesseract/tessdata \
-	     --save_box_tiff \
-	     --maxpages 10 \
-	     --output_dir train
+ --fontlist 'PspimpdeedII' 'PspimpdeedIII' 'LYF_Paint' 'KoneRomiCuteThin' \
+ --lang tha \
+ --linedata_only \
+ --langdata_dir langdata_lstm \
+ --tessdata_dir /Users/babynong/pictotect/tessdata_best/ \
+ --save_box_tiff \
+ --maxpages 30 \
+ --output_dir train
